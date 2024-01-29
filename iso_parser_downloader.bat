15eaaae36e521d9c2938f5408aa88b074462f902
Hossein Assaran <hossein.assaran@gmail.com>
2024-01-29 21:01:15 +0330

    Update execute parser downloader

--- iso_parser_downloader.bat
+++ iso_parser_downloader.bat
@@ -1,10 +1,10 @@
 @echo off
 
 REM Set the URL of the file to download
-set "url=https://github.com/HosseinAssaran/ISO8583-Parser/releases/download/v0.1.8/emv_parser.exe"
+set "url=https://github.com/HosseinAssaran/ISO8583-Parser/releases/download/v0.1.8/iso8583_parser.exe"
 
 REM Set the absolute destination path for the downloaded file
-set "destination=%~dp0/target/release/mv_parser.exe"
+set "destination=%~dp0/target/release/iso8583_parser.exe"
 
 REM Download the file using bitsadmin
 bitsadmin /transfer myDownloadJob /download /priority normal %url% %destination%
