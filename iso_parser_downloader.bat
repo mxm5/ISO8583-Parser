43ec0837f7b9e664943ba345674eddd1259b0386
Hossein Assaran <hossein.assaran@gmail.com>
2024-01-29 20:49:42 +0330

    add parser downloader to project to save memory on git

---
+++ iso_parser_downloader.bat
@@ -0,0 +1,15 @@
+@echo off
+
+REM Set the URL of the file to download
+set "url=https://github.com/HosseinAssaran/ISO8583-Parser/releases/download/v0.1.5/emv_parser.exe"
+
+REM Set the absolute destination path for the downloaded file
+set "destination=%~dp0/target/release/mv_parser.exe"
+
+REM Download the file using bitsadmin
+bitsadmin /transfer myDownloadJob /download /priority normal %url% %destination%
+
+REM Wait for the download to complete (optional)
+REM timeout /t 10 /nobreak
+
+echo Download complete.
